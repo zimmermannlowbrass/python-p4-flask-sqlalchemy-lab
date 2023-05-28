f92d5a15dd4486442a68be940c5e9ca442eb2b0f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-28 12:08:42 -0500

    completed labwork

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Zookeeper, Enclosure, Animal
+from models import db, Animal, Zookeeper, Enclosure
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,16 +19,47 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if not animal:
+        response_body = '<h1>404 not found </h1>'
+
+    response_body = f''
+    response_body += f'<ul>ID: {animal.id}</ul>'
+    response_body += f'<ul>Name: {animal.name}</ul>'
+    response_body += f'<ul>Species: {animal.species}</ul>'
+    response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+    response_body += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        response_body = '<h1>404 not found </h1>'
+
+    response_body = f'<ul>Name: {zookeeper.name}</ul> \
+                    <ul>Birthday: {zookeeper.birthday}</ul>'
+    
+    animals = Animal.query.filter(Animal.zookeeper_id == zookeeper.id).all()
+    for animal in animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f''
+    response_body += f'<ul>ID: {enclosure.id}</ul>'
+    response_body += f'<ul>Environment: {enclosure.environment}</ul>'
+    response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
 
+    animals = Animal.query.filter(Animal.enclosure_id == enclosure.id).all()
+    for animal in animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+    response = make_response(response_body)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,13 +11,26 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.String)
+
+    animal = db.relationship('Animal', backref='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+
+    animal = db.relationship('Animal', backref='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
